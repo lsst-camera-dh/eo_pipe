4d4737328ef90b70069eae802294929b0b53a5e0
James Chiang <jchiang87@gmail.com>
2023-04-10 11:31:33 -0700

    read header data with astropy.io.fits

--- python/lsst/eo/pipe/runInfoTask.py
+++ python/lsst/eo/pipe/runInfoTask.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from astropy.io import fits
 import pandas as pd
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
@@ -28,6 +29,10 @@ class RunInfoTaskConfig(pipeBase.PipelineTaskConfig,
     metadata_fields = pexConfig.ListField(
         doc="Keyword names of values to extract from raw exposure metadata.",
         dtype=str, default=[])
+    use_astropy = pexConfig.Field(
+        doc="Flag to use astropy.io.fits to read the FITS header "
+        "instead of using the .getMetadata function.",
+        dtype=bool, default=True)
 
 
 class RunInfoTask(pipeBase.PipelineTask):
@@ -41,6 +46,7 @@ class RunInfoTask(pipeBase.PipelineTask):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.keys = self.config.metadata_fields
+        self.use_astropy = self.config.use_astropy
 
     def run(self, raws):
         data = defaultdict(list)
@@ -55,7 +61,12 @@ class RunInfoTask(pipeBase.PipelineTask):
             data['mjd_begin'].append(timespan.begin.mjd)
             data['mjd_end'].append(timespan.end.mjd)
             if self.keys:
-                md = handle.get().getMetadata()
+                if self.use_astropy:
+                    filepath = handle.butler.getURI(handle.ref).path
+                    with fits.open(filepath) as hdus:
+                        md = dict(hdus[0].header)
+                else:
+                    md = handle.get().getMetadata()
                 for key in self.keys:
                     data[key].append(md.get(key, None))
         df = pd.DataFrame(data).sort_values(by=['detector', 'mjd_begin'])
