4b57b7c39b19847f9909e03cdb1fdb98a6187eb8
James Chiang <jchiang87@gmail.com>
2023-07-08 13:45:55 -0700

    function to copy transmission_sensor data from one repo to another

--- python/lsst/eo/pipe/ingest_utils.py
+++ python/lsst/eo/pipe/ingest_utils.py
@@ -3,7 +3,29 @@ import lsst.daf.butler as daf_butler
 from lsst.resources import ResourcePath
 
 
-__all__ = ['ingest_pd_data']
+__all__ = ['ingest_pd_data', 'copy_ts_data']
+
+
+def copy_ts_data(src_repo, in_collection, dest_repo, out_collection):
+    """
+    Copy transmission_sensor data from one repo to another, writing
+    out to the latter in the specified run collection.
+    """
+    src_butler = daf_butler.Butler(src_repo, collections=[in_collection])
+    refs = list(set(src_butler.registry.queryDatasets('transmission_sensor',
+                                                      findFirst=True)))
+
+    dest_butler = daf_butler.Butler(dest_repo, writeable=True)
+    dest_butler.registry.registerCollection(
+        out_collection, daf_butler.registry.CollectionType.RUN)
+
+    for ref in refs:
+        ts = src_butler.get(ref)
+        try:
+            dest_butler.put(ts, 'transmission_sensor', ref.dataId,
+                            run=out_collection)
+        except daf_butler.registry.ConflictingDefinitionError:
+            print("ConflictingDefinitionError:", ref.dataId)
 
 
 def ingest_pd_data(acq_run, instrument='LSSTCam', output_run=None,
