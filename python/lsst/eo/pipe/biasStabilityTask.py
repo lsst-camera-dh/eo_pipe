c1ee97944424dce46aa8b9f9e014bdcf20700a31
James Chiang <jchiang87@gmail.com>
2024-03-05 15:41:20 -0800

    use an instance of IsrTask internally instead of running isr separately

--- python/lsst/eo/pipe/biasStabilityTask.py
+++ python/lsst/eo/pipe/biasStabilityTask.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import lsst.afw.math as afwMath
 import lsst.geom
+from lsst.ip.isr import IsrTask, IsrTaskConfig
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
 from lsst.pipe.base import connectionTypes as cT
@@ -64,27 +65,50 @@ def get_readout_corner(image, amp, raw_amp, dxy):
 
 class BiasStabilityTaskConnections(pipeBase.PipelineTaskConnections,
                                    dimensions=("instrument", "detector")):
-    exposures = cT.Input(name="postISRCCD",
-                         doc="CCDs with desired level of ISR applied.",
-                         storageClass="Exposure",
-                         dimensions=("instrument", "exposure", "detector"),
-                         multiple=True,
-                         deferLoad=True)
+
+    raws = cT.Input(name="raw",
+                    doc="Input exposure to process.",
+                    storageClass="Exposure",
+                    dimensions=["instrument", "exposure", "detector"],
+                    multiple=True,
+                    deferLoad=True)
+
+    biases = cT.PrerequisiteInput(
+        name="cpBiasProc",
+        doc="Input bias calibration.",
+        storageClass="ExposureF",
+        dimensions=["instrument", "detector"],
+        multiple=True,
+        deferLoad=True,
+        isCalibration=True)
+
+    darks = cT.PrerequisiteInput(
+        name='cpDarkProc',
+        doc="Input dark calibration.",
+        storageClass="ExposureF",
+        dimensions=["instrument", "detector"],
+        multiple=True,
+        deferLoad=True,
+        isCalibration=True)
+
     camera = cT.PrerequisiteInput(name="camera",
                                   doc="Camera used in observations",
                                   storageClass="Camera",
                                   isCalibration=True,
                                   dimensions=("instrument",))
+
     bias_stability_stats = cT.Output(name="bias_stability_stats",
                            doc="bias stability statistics",
                            storageClass="DataFrame",
                            dimensions=("instrument", "detector"))
+
     bias_serial_profile_plots = cT.Output(
         name="bias_serial_profile_plots",
         doc="profiles of median per-column bias "
         "level as a function of serial pixel",
         storageClass="Plot",
         dimensions=("instrument", "detector"))
+
     bias_parallel_profile_plots = cT.Output(
         name="bias_parallel_profile_plots",
         doc="profiles of median per-column bias "
@@ -92,14 +116,40 @@ class BiasStabilityTaskConnections(pipeBase.PipelineTaskConnections,
         storageClass="Plot",
         dimensions=("instrument", "detector"))
 
+    def __init__(self, *, config=None):
+        super().__init__(config=config)
+
+        if not config.doBias:
+            self.prerequisiteInputs.remove("biases")
+
+        if not config.doDark:
+            self.prerequisiteInputs.remove("darks")
+
 
 class BiasStabilityTaskConfig(pipeBase.PipelineTaskConfig,
                               pipelineConnections=BiasStabilityTaskConnections):
-    ncol_skip = pexConfig.Field(doc="Number of initial and trailing columns "
-                                "to skip in evaluating serial overscan level",
-                                dtype=int, default=4)
-    oscan_method = pexConfig.Field(doc="Serial overscan subtraction method",
-                                   dtype=str, default="median_per_row")
+    doBias = pexConfig.Field(doc="Apply bias subtraction",
+                             dtype=bool, default=False)
+    doDark = pexConfig.Field(doc="Apply dark subtraction",
+                             dtype=bool, default=False)
+    overscan_fitType = pexConfig.Field(doc="Overscan fit type",
+                                       dtype=str, default="MEDIAN_PER_ROW")
+    overscan_leadingColumnsToSkip = pexConfig.Field(
+        doc="Number of leading columns to skip for overscan correction",
+        dtype=int, default=4)
+    overscan_trailingColumnsToSkip = pexConfig.Field(
+        doc="Number of trailing columns to skip for overscan correction",
+        dtype=int, default=4)
+    overscan_leadingRowsToSkip = pexConfig.Field(
+        doc="Number of leading rows to skip for overscan correction",
+        dtype=int, default=4)
+    overscan_trailingRowsToSkip = pexConfig.Field(
+        doc="Number of trailing rows to skip for overscan correction",
+        dtype=int, default=0)
+    overscan_doParallelOverscan = pexConfig.Field(
+        doc="Apply parallel overscan correction.",
+        dtype=bool, default=False)
+
     readout_corner_size = pexConfig.Field(doc="Size of readout corner region "
                                           "to consider, in pixels.",
                                           dtype=int, default=200)
@@ -123,15 +173,40 @@ class BiasStabilityTask(pipeBase.PipelineTask):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.ncol_skip = self.config.ncol_skip
-        self.oscan_method = self.config.oscan_method
         self.readout_size = self.config.readout_corner_size
         self.nsigma = self.config.nsigma
         self.nsigma_y_axis = self.config.nsigma_y_axis
         self.figsize = self.config.xfigsize, self.config.yfigsize
+        self.isr_config = IsrTaskConfig()
+        self.isr_config.doBias = self.config.doBias
+        self.isr_config.doDark = self.config.doDark
+        self.isr_config.overscan.fitType = self.config.overscan_fitType
 
-    def run(self, exposures, camera):
-        raw_det = camera[exposures[0].dataId['detector']]
+        self.isr_config.overscan.leadingColumnsToSkip \
+            = self.config.overscan_leadingColumnsToSkip
+        self.isr_config.overscan.leadingRowsToSkip \
+            = self.config.overscan_leadingRowsToSkip
+
+        self.isr_config.overscan.trailingColumnsToSkip \
+            = self.config.overscan_trailingColumnsToSkip
+        self.isr_config.overscan.trailingRowsToSkip \
+            = self.config.overscan_trailingRowsToSkip
+
+        self.isr_config.overscan.doParallelOverscan \
+            = self.config.overscan_doParallelOverscan
+        self.isr_config.doFlat = False
+        self.isr_config.doBrighterFatter = False
+        self.isr_config.doApplyGains = False
+        self.isr_config.doLinearize = False
+        self.isr_config.doFringe = False
+        self.isr_config.doDefect = False
+        self.isr_config.doSaturation = False
+        self.isr_config.maskListToInterpolate=['SAT', 'BAD', 'UNMASKEDNAN']
+        self.log.info('IsrTask config:\n')
+        self.log.info(self.isr_config.saveToString())
+
+    def run(self, raws, biases=None, darks=None, camera=None):
+        raw_det = camera[raws[0].dataId['detector']]
         namps = len(raw_det)
         data = defaultdict(list)
         profile_plots = {'serial': plt.figure(figsize=self.figsize),
@@ -141,8 +216,11 @@ class BiasStabilityTask(pipeBase.PipelineTask):
                 for key, plot in profile_plots.items()}
         values = {key: {amp: [] for amp in range(1, namps+1)}
                   for key in profile_plots}
-        for handle in exposures:
-            exp = handle.get()
+        for handle in raws:
+            isr_task = IsrTask()
+            isr_task.config = self.isr_config
+            raw = handle.get()
+            exp = isr_task.run(raw).exposure
             image = exp.getImage()
             md = exp.getMetadata()
             det = exp.getDetector()
