409386f2da5918882ad9b5002bebf55822978360
James Chiang <jchiang87@gmail.com>
2024-08-26 10:57:13 -0700

    change default repo from ir2 to embargo for ingest_pd_data.py; enable the S3 bucket name to be specified

--- bin.src/ingest_pd_data.py
+++ bin.src/ingest_pd_data.py
@@ -9,10 +9,13 @@ parser.add_argument("--instrument", type=str, default="LSSTCam",
                     help="Instrument, e.g., LSSTCam, LSST-TS8")
 parser.add_argument("--output_run", type=str, default=None,
                     help="Run collection to contain photodiode data")
-parser.add_argument("--repo", type=str, default="/repo/ir2",
+parser.add_argument("--repo", type=str, default="/repo/embargo",
+                    help="Data repository")
+parser.add_argument("--s3_bucket", type=str, default="rubin-summit",
                     help="Data repository")
 
 args = parser.parse_args()
 
 ingest_pd_data(args.run, instrument=args.instrument,
-               output_run=args.output_run, repo=args.repo)
+               output_run=args.output_run, repo=args.repo,
+               s3_bucket=args.s3_bucket)
