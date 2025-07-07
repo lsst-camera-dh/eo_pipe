d3b2f304d4835b8fafe1c9ed801301423f867ab3
James Chiang <jchiang87@gmail.com>
2025-07-06 21:04:06 -0700

    code to infer max_frac_dev from spline model of linearity

--- python/lsst/eo/pipe/linearizerPlotsTask.py
+++ python/lsst/eo/pipe/linearizerPlotsTask.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import numpy as np
+import lsst.afw.image as afw_image
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
 from lsst.pipe.base import connectionTypes as cT
@@ -8,6 +10,62 @@ from .plotting import plot_focal_plane, hist_amp_data, append_acq_run
 
 __all__ = ['LinearizerFpPlotsTask']
 
+
+def infer_max_frac_devs(linearizer, ptc, det, Ne_min=1e3, Ne_max=9e4):
+    """
+    Back out the maximum fractional deviation from a linear fit using
+    the cp_pipe linearizer object for each amp in a detector.  This
+    function assumes a spline model for the linearity correction was
+    used.
+    """
+    max_frac_devs = {}
+    for amp in det:
+        amp_name = amp.getName()
+        # Extract the measured ADU values and fit residuals.
+        yvals = ptc.rawMeans[amp_name]
+        # Replace nans in the fit residuals with zeros.
+        resids = np.zeros(len(yvals))
+        for i, value in enumerate(linearizer.fitResiduals[amp_name]):
+            if value == value:
+                resids[i] = value
+
+        # Create an uncorrected image with a ramp covering the
+        # measured ADU values from which to infer the measure signal
+        # vs incident (corrected) flux (used as proxy for pd integrals).
+        bbox = linearizer.linearityBBox[amp_name]
+        image = afw_image.ImageF(bbox.width, bbox.height)
+        # Subtract off the fit residuals so that measured points lie
+        # on the spline-fitted linearity curve.
+        meas_min = min(yvals - resids)
+        meas_max = max(yvals - resids)
+        meas = np.logspace(np.log10(meas_min)*np.ones(bbox.width),
+                           np.log10(meas_max)*np.ones(bbox.width),
+                           bbox.height,
+                           axis=0)
+        # Apply the linearity correction.
+        image.array[:] = meas.copy()
+        coeffs = linearizer.linearityCoeffs[amp_name]
+        correction_func = linearizer.getLinearityTypeByName(
+            linearizer.linearityType[amp_name])()
+        gain = ptc.gain[amp_name]
+        correction_func(image=image, coeffs=coeffs, gain=gain)
+        corr = image.array.copy()
+        # Evaluate the corrected flux values at the measured ADU values.
+        xvals = np.interp(yvals - resids, meas[:, 0], corr[:, 0])
+        # Select points within the desired fit range.
+        index = np.where((1e3 < yvals*gain)*(yvals*gain < 9e4))
+        # Fit line with zero y-intercept.
+        denom = sum(xvals[index]**2/yvals[index])
+        if denom == 0:
+            continue
+        aa = sum(xvals[index])/denom
+        def func(x):
+            return aa*x
+        # Compute the fractional residuals to this fit.
+        frac_resids = (yvals - func(xvals))/yvals
+        max_frac_devs[amp_name] = float(max(np.abs(frac_resids)))
+
+    return max_frac_devs
 
 class LinearizerFpPlotsTaskConnections(pipeBase.PipelineTaskConnections,
                                        dimensions=("instrument",)):
@@ -111,12 +169,16 @@ class LinearizerFpPlotsTask(pipeBase.PipelineTask):
             det_name = det.getName()
             linearizer = linearizer_map[detector].get()
             ptc = ptc_map[detector].get()
+            max_frac_devs = infer_max_frac_devs(linearizer, ptc, det)
             for amp in det:
                 amp_name = amp.getName()
                 frac_defs = (linearizer.fitResiduals[amp_name]
                              / ptc.rawMeans[amp_name])
-                amp_data['max_frac_dev'][det_name][amp_name] \
-                    = float(max(frac_defs))
+                try:
+                    amp_data['max_frac_dev'][det_name][amp_name] \
+                        = max_frac_devs[amp_name]
+                except KeyError:
+                    pass
                 amp_data['max_observed_signal'][det_name][amp_name] \
                     = float(linearizer.linearityMaxSignal[amp_name])
                 amp_data['linearity_turnoff'][det_name][amp_name] \
